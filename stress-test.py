4a88db302e13588cc299fab8c4f58c4f21fe3b0f
Philip Basford <pjb@ecs.soton.ac.uk>
2015-02-25 22:07:32 +0000

    Now attempts to write to serial port and quits cleanly if unavailable

--- stress-test.py
+++ stress-test.py
@@ -3,45 +3,53 @@ import serial
 from optparse import OptionParser, OptionGroup 
 from time import sleep
 
-VERSION_NUMBER = "0.0.1"
 DEFAULT_DEVICE = "/dev/ttyUSB0"
 DEFAULT_BAUD = 38400
 DEFAULT_SLEEP = 1
 TIMEMOUT = 0.1
 
 
-def stress_test(device, baud, sleep):
-	print("Stress testing rtiduino controller on port %s baudrate %s" % (device, baud))
-	print("Sleep time set to %d" % sleep)
-	ser = serial.Serial(device, baud, timeout=TIMEMOUT)
-	while(True):
-		ser.write("AQBWCE")
+def stress_test(device, baud, sleep_time):
+    print("Stress testing rtiduino controller on port %s baudrate %s" % (device, baud))
+    print("Sleep time set to %d" % sleep_time)
+    try:
+        ser = serial.Serial(device, baud, timeout=TIMEMOUT)
+    except serial.serialutil.SerialException as e:
+        print e
+        exit(1)
+    while(True):
+        ser.write("AQBWCE")
+        sleep(sleep_time)
 
 
 def main():
-    parser = Optionparser(version="%prog %s"% VERSION_NUMBER)
+    parser = OptionParser(version="%prog 0.0.1")
     parser.add_option("-d", "--device", action="store",
-    	type="string", dest="device",
-    	help="The serial port to use")
+        type="string", dest="device",
+        help="The serial port to use")
     parser.add_option("-b", "--baud", action="store",
-    	type="string", dest="baud",
-    	help="The baudrate to use")
+        type="string", dest="baud",
+        help="The baudrate to use")
     parser.add_option("-s", "--sleep", action="store",
-    	type="string", dest="sleep",
-    	help="The amount of time to sleep between sucessive messages")
+        type="string", dest="sleep",
+        help="The amount of time to sleep between sucessive messages")
     (options, args) = parser.parse_args()
    
     if options.device is None:
-    	device = DEFAULT_DEVICE
+        device = DEFAULT_DEVICE
     else:
-    	device = options.device
-	if options.baud is None:
-		baud = DEFAULT_BAUD
-	else:
-		baud = options.baud
-	stress_test(device, baud)
+        device = options.device
+    if options.baud is None:
+        baud = DEFAULT_BAUD
+    else:
+        baud = options.baud
+    if options.sleep is None:
+        sleep_time = float(DEFAULT_SLEEP)
+    else:
+        sleep_time = float(options.sleep)
+    stress_test(device, baud, sleep_time)
 
 
 
 if __name__ == "__main__":
-	main()
+    main()
