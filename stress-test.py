baa2173df76e7958ddb49b17e7865a23cf585109
Philip Basford <pjb@ecs.soton.ac.uk>
2015-02-26 15:17:28 +0000

    Now tests arduino and stops if a failure is detected

--- stress-test.py
+++ stress-test.py
@@ -2,29 +2,44 @@
 import serial
 from optparse import OptionParser, OptionGroup 
 from time import sleep
+from datetime import datetime
 
 DEFAULT_DEVICE = "/dev/ttyUSB0"
 DEFAULT_BAUD = 38400
-DEFAULT_SLEEP = 1
-TIMEMOUT = 0.1
+DEFAULT_SLEEP = 0.01
+TIMEMOUT = 0.01
 
 
 def stress_test(device, baud, sleep_time):
     print("Stress testing rtiduino controller on port %s baudrate %s" % (device, baud))
-    print("Sleep time set to %d" % sleep_time)
+    print("Sleep time set to %f" % sleep_time)
     try:
         ser = serial.Serial(device, baud, timeout=TIMEMOUT)
     except serial.serialutil.SerialException as e:
         print e
         exit(1)
+    i = 0
+    start = datetime.utcnow()
     while(True):
         ser.write("AQBWCE")
+        resp = str(ser.readline())
+        if not resp.startswith("OK"):
+            print "ERROR: %s" % resp
+            break
+        else:
+            i = i+1
+        if i%500 == 0:
+            print "Count = %d" % i
+
         sleep(sleep_time)
+    print "Count = %d" % i
+    print "Failed at %s" % datetime.utcnow()
+    print "Ran for %s" % (datetime.utcnow() - start)
 
 
 def main():
     parser = OptionParser(version="%prog 0.0.1")
-    parser.add_option("-d", "--device", action="store",
+    parser.add_option("-D", "--device", action="store",
         type="string", dest="device",
         help="The serial port to use")
     parser.add_option("-b", "--baud", action="store",
